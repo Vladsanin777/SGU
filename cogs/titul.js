c3565f584a537219e68e6c36170620f14bb29cfb
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2023-12-29 22:58:43 +0400

    m

--- cogs/titul.js
+++ cogs/titul.js
@@ -1,12 +1,18 @@
 class ALL_DATA {
     static backgroundColor_all;
-    static color_all;
+    static color_all = ["red", "blue", "green", "pink", "maroon", "orange", "olive", "yellow", "lime", "teal", "cyan", "aqua", "purple", "fuchsia"];
+    static button = ["Для абитуриентов", "Для студентов"];
     static width_all;
     static height_all;
+    static colorText;
+    static colorView;
+    static colorBackground;
+    static button_list;
+    static group_button_list;
 }
 class ALL {
     // This method creates a button with the specified name, width, height, color, and background color
-    createButton(name, buttonWidth, buttonHeight, color, backgroundColor) {
+    createButton(name) {
         const button = document.createElement("button");
         button.textContent = name; // Set the button's text content to the specified name
         button.style.display = "inline"; // Set the button's display property to inline
@@ -24,52 +30,66 @@ class ALL {
 
     // This method generates a random index from the colors array
     random_color() {
-        return Math.floor(Math.random() * colors.length); // Get a random integer between 0 and the length of the colors array
+        all_data = new ALL_DATA()
+        return Math.floor(Math.random() * all_data.colors.length); // Get a random integer between 0 and the length of the colors array
+    }
+    setupBackground(){
+        all_data = new ALL_DATA()
+        // Устанавливаем фоновый цвет в соответствии с случайным числом
+        document.body.style.backgroundColor = all_data.colorBackground;
+    }
+    setupColor_all() {
+        all_data = new ALL_DATA()
+        // Generate random colors for the button text, view, and background
+        let colorText = this.random_color();
+        let colorView = this.random_color(); 
+        while (colorView == colorText) {
+            colorView = this.random_color(); 
+        }
+        let colorBackground = this.random_color();
+        while (colorText == colorBackground | colorView == colorBackground) {
+            colorBackground = this.random_color(); 
+        color = all_data.color;
+        all_data.colorBackground = color[colorBackground];
+        all_data.colorView = color[colorView];
+        all_data.colorText = color[colorText];
+        }
+    }
+    get_window(){
+        all_data = new ALL_DATA();
+        all_data.height_all = window.innerHeight;
+        all_data.width_all =window.innerWidth;
+    }
+    buttonwindow(){
+        all_data = new ALL_DATA();
+        width_all = all_data.width_all
+        
+    }
+    createButton_setup(){
+        all_data = new ALL_DATA();
+        // Создаем кнопки
+        button = all_data.button
+        for ( i in button){
+            button_list.push(this.createButton(i))
+        }
+        all_data.button_list = button_list
+    }
+    createGroup(){
+        all_data = new ALL_DATA();
+        // Добавляем кнопки в группу
+        const buttonGroup = document.createElement("div");
+        buttonGroup.style.display = "flex";
+        buttonGroup.style.justifyContent = "center";
+        buttonGroup.style.alignItems = "center";
+        buttonGroup.appendChild(buttonForAbbiturient);
+        buttonGroup.appendChild(buttonForStudens);
+    }
+    createGroup_setup(){
+        all_data = new ALL_DATA();
+        group_button_list = all_data.group_button_list
+        // Добавляем группу кнопок на страницу
+        for (i in group_button_list){
+            document.body.appendChild(i);
+        }
     }
 }
-
-// Define the colors array
-const colors = ["red", "blue", "green", "pink", "maroon", "orange", "olive", "yellow", "lime", "teal", "cyan", "aqua", "purple", "fuchsia"];
-
-// Generate random colors for the button text, view, and background
-const colorText = new ALL().random_color();
-const colorView = new ALL().random_color(); // Get a random color for the button's view
-while (colorView == colorText) {
-colorView = new ALL().random_color(); // If the view color is the same as the text color, generate a new random color
-}
-const colorBackground = new ALL().random_color(); // Get a random color for the button's background
-while (colorIndex == colorBackground) {
-colorBackground = new ALL().random_color(); // If the background color is the same as the index color, generate a new random color
-}
-
-// Устанавливаем фоновый цвет в соответствии с случайным числом
-document.body.style.backgroundColor = colors[colorIndex];
-
-// Объявляем переменные для ширины и высоты кнопки
-let buttonWidth;
-let buttonHeight;
-
-// Получаем ширину и высоту окна
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-
-console.log(windowWidth, windowHeight);
-
-// Рассчитываем ширину и высоту кнопки
-buttonWidth = windowWidth / 3;
-buttonHeight = windowHeight / 6;
-const view = new View_1();
-// Создаем кнопки
-const buttonForAbbiturient = view.createButton("Для абитуриентов", buttonWidth, buttonHeight, colors[colorIndex], colors[colorView]);
-const buttonForStudens = view.createButton("Для студентов", buttonWidth, buttonHeight, colors[colorIndex], colors[colorView]);
-
-// Добавляем кнопки в группу
-const buttonGroup = document.createElement("div");
-buttonGroup.style.display = "flex";
-buttonGroup.style.justifyContent = "center";
-buttonGroup.style.alignItems = "center";
-buttonGroup.appendChild(buttonForAbbiturient);
-buttonGroup.appendChild(buttonForStudens);
-
-// Добавляем группу кнопок на страницу
-document.body.appendChild(buttonGroup);
